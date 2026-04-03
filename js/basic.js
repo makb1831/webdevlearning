e14b8c80ed5a26f8f83a531e7380162bae992885
makb1831 <muhammad.adnan@uoj.edu.pk>
2026-04-03 12:40:05 +0500

    Enhance JS variables teaching sample with additional examples and control flow demonstrations

--- js/basic.js
+++ js/basic.js
@@ -1,12 +1,13 @@
 ﻿/* basic.js - JS variables teaching sample */
 
 // 1) var: function-scoped (or globally scoped if outside function)
-function testVarScope() {
+function testVarScope() { 
     var x = 10;
     if (true) {
         var x = 20; // same variable as above
         console.log('inside if var x =', x); // 20
     }
+    
     console.log('after if var x =', x); // 20
 }
 
@@ -71,3 +72,194 @@ function showType(value) {
 console.log('showType(num):', showType(num));
 console.log('showType(str):', showType(str));
 testConstScope(300);
+p = false;
+x = 6;
+px = 6;
+c = (x===px);
+console.log('p == x:', c)
+for(let i=0; i<5; i++) {
+    console.log('i =', i);
+}
+while (x > 0) {
+    console.log('x =', x);
+    x--;
+    console.log('x after decrement =', x);
+}
+do {
+    console.log('x in do-while =', x);
+} while (x > 0);
+f = 0;
+a = 1;
+switch(f+a+6){
+    case 0:
+        console.log('f is zero');
+        break;
+    case 1:
+        console.log('f is one');
+        break;
+    case '7':
+        console.log('f is seven');
+        break;
+    default:
+        console.log('f is something else');
+}
+
+for(let i=0; i<5; i++) {
+    if(i === 3) {
+        console.log('Breaking loop at i =', i);
+        continue; // skip the rest of this iteration and continue with next
+    }
+    if(i % 2 === 0) {
+        console.log('even i =', i);
+    } else {
+        console.log('odd i =', i);
+    }
+    
+}
+for(let i=0; i<5; i++) {
+    x = 'a';
+    switch(x) {
+        case 'a':
+            console.log('x is a');
+        case 0:
+        case 2:
+        case 4:
+            console.log('even i =', i);
+            break;
+        case 1:
+        case 3:
+            console.log('odd i =', i);
+            break;
+        default:
+            console.log('x is something else');
+    }
+}
+a=1;
+if(a==1) {
+    console.log('Monday');
+}
+else if(a==2) {
+    console.log('Tuesday');
+}
+else if(a==3) {
+    console.log('Wednesday');
+}
+else if(a==4) {
+    console.log('Thursday');
+}
+else if(a==5) {
+    console.log('Friday');
+}
+else if(a==6) {
+    console.log('Saturday');
+}
+else if(a==7) {
+    console.log('Sunday');
+}
+switch(a) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+    case 3:
+        console.log('Wednesday');
+        break;
+    default:
+        console.log('Other day');
+}
+if(a==1 || a==2){
+    console.log('Week starting days');
+} else if(a==3 || a==4 || a==5) {
+    console.log('Week middle days');
+} else if(a==6 || a==7) {
+    console.log('Weekend days');
+} else {
+    console.log('Invalid day');
+}
+if(a==1){
+    console.log('Week starting day');
+} else if(a==2){
+    console.log('Week starting day');
+} else if(a==3) {
+    console.log('Week middle day');
+} else if(a==4) {
+    console.log('Week middle day');
+} else if(a==5) {
+    console.log('Week middle day');
+} else if(a==6) {
+    console.log('Weekend day');
+} else if(a==7) {
+    console.log('Weekend day');
+} else {
+    console.log('Invalid day');
+}
+switch(a) {
+    case 1:
+    case 2:
+        console.log('Week starting days');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Week middle days');
+        break;
+    case 6:
+    case 7:
+        console.log('Weekend days');
+        break;
+    default:
+        console.log('Invalid day');
+}
+a=4;
+x=2;
+if(a>=1 && a<=7) {
+   if(a>1){
+    x++;
+    if(a>2){
+        x++;
+        if(a>3){
+            x++;
+            if(a>4){
+                x++;
+                if(a>5){
+                    x++;
+                    if(a>6){
+                        console.log('nested Weekend day');
+                    } else {
+                        x++;
+                        console.log('nested Week middle day');
+                    }
+                } else {
+                    console.log('nested Week middle day');
+                }
+            } else {
+                console.log('nested Week middle day');
+            }
+        } else {
+            console.log('nested Week starting day');
+        }
+    } else {
+        console.log('nested Week starting day');
+    }
+
+   } else {
+       console.log('nested Week starting day');
+   }
+}
+console.log(x);
+b=2;
+if(b>=1){
+    if(b>1){
+        if(b==2){
+            console.log('b is equal to 2');
+        }
+    } 
+    if(b%2==0){
+        console.log('b is even');
+    }
+}
+p = 'ali';
+t = 'khan';
+p += t;
+console.log(p);
